667671744d253f11d806e3f87039a80fa47c25de
Teija Stearns <trs@datahost.com>
2016-04-27 16:28:40 -0700

    new/fixed fullscreen

--- demo/js/aem-mobile-transition.js
+++ demo/js/aem-mobile-transition.js
@@ -26,7 +26,12 @@
                     if (opts.mode=='default'){
                         $(article).css({'z-index':1000,'opacity':1}).addClass('ani ani-appear');
                     } else if (opts.mode!='default') {
-                        $(cardObj.relatedTarget).addClass('aem-fullscreen-wrapper ani ani-appear');
+                        $(cardObj.relatedTarget).addClass('aem-fullscreen-wrapper bring-to-front ani ani-appear');
+                        $('#cardWrapper').css({
+                            'top': '',
+                            'width':'',
+                            'height':''
+                        }).addClass('aem-fullscreen-wrapper').removeClass('ani ani-expand');
                         $(cardObj.target).addClass('vcenter');
                         $(opts.back).fadeIn();
                     }
@@ -36,7 +41,6 @@
                         cardObj.aemContract();
                     } else if (opts.mode!='default') {
                         $(this).fadeOut();
-                        console.log(article);
                         if (cardObj.mode=='video'){
                             $(article)[0].pause();
                         }
@@ -59,7 +63,6 @@
 
     function Card(myCard,opts) {
         var cords = myCard.getBoundingClientRect();
-        console.log(cords);
         var winH = $(window).height();
         var winW = $(window).width();
         var x = cords.left;
@@ -103,16 +106,6 @@
                 'transform': 'translate(' + this.xTransBack + 'px, ' + this.yTransBack + 'px)scale(' + this.xScaleDown + ',' + this.yScaleDown + ')'
             }
         }]);
-        
-//        $.keyframe.define([{
-//            name: 'fullscreen-contract',
-//            'from': {
-//                 'transform': 'translate3d(' + this.xTrans + 'px, ' + this.yTrans + 'px,0)scale(' + this.wScale + ',' + this.hScale + ')'
-//            },
-//            'to': {
-//                'transform': 'translate(0,0),scale(1,1)'
-//            }
-//        }]);
     }
     
     Card.prototype.aemExpand = function () {
@@ -131,16 +124,18 @@
     }
     
     Card.prototype.fullscreen = function () {
-        $(article).removeClass('ani ani-appear');
         $(article).trigger('aem.expand');
-        var placeholder = $('<div id="placeholder"></div>').width(this.w).height(this.h).css({'top': this.q.top});
-        var cardWrapper = $('<div class="ani ani-expand" id="cardWrapper"></div>').width(this.w).height(this.h).css({'top': this.q.top,'background':'#000'});
+        var cardWrapper = $('<div class="ani ani-expand" id="cardWrapper"></div>').css({
+            'top': this.q.top,
+            'background':'#000',
+            'width':this.w,
+            'height':this.h
+        });
         $('body').addClass('article-open');
         $(cardWrapper).insertAfter(this.relatedTarget);
         $('#cardWrapper').on('animationend webkitAnimationEnd',function(e){
             if(e.originalEvent.animationName=='canvas-expand'){
                 $(article).trigger('aem.expanded');
-                $(placeholder).insertAfter(cardObj.relatedTarget);
             };
         });
     }
@@ -162,9 +157,7 @@
                     }).removeClass('ani ani-contract');
                 }, 320);
             };
-            
-//            console.log(e.type,e.originalEvent.animationName);
-//            
+                  
 //            if (e.type == 'animationend' || e.type == 'webkitAnimationEnd') {
 //                console.log(e.originalEvent.animationName);
 //            };
@@ -172,43 +165,22 @@
     }
 
 Card.prototype.exitFullscreen = function () {
-        $('#cardWrapper').remove();
-        $(article).addClass('ani ani-fade');
-        $(this.relatedTarget).removeClass('ani ani-appear').addClass('ani ani-exit-fullscreen');
-//        $('#cardWrapper').removeClass('ani ani-expand').addClass('ani-exit-fullscreen');
+        $(this.relatedTarget).removeClass('ani ani-appear aem-fullscreen-wrapper bring-to-front').addClass('ani ani-fade');
+        $('#cardWrapper').addClass('ani ani-exit-fullscreen');
         $(this.relatedTarget).on("webkitAnimationEnd animationend", function (e) {
              if (e.originalEvent.animationName == 'fade-out'){
                  setTimeout(function(){
-                     $(article).removeClass('ani ani-fade vcenter').fadeIn();
-                     $(cardObj.relatedTarget).removeClass('aem-fullscreen-wrapper ani ani-exit-fullscreen');
-                     $('#placeholder').remove();
-                 }, 400);
-//                 $('.aem-wrapper').hide().removeClass('aem-fullscreen-wrapper');
-//                $('#cardWrapper').removeClass('ani ani-expand').addClass('ani ani-contract');
+                     $(cardObj.target).removeClass('vcenter').fadeIn();
+                     $(cardObj.relatedTarget).removeClass('ani ani-fade');
+                     $('#cardWrapper').fadeOut();
+                     setTimeout(function(){
+                         $('#cardWrapper').remove();
+                     },200);
+                 }, 150);
+
              }
         });
     
-//    $(article).on("webkitAnimationStart animationstart webkitAnimationEnd animationend", function (e) {
-//            if (e.originalEvent.animationName == 'card-contract' && (e.type == 'animationstart' || e.type == 'webkitAnimationStart')) {
-//                setTimeout(function () {
-//                    $(cardClone).removeClass('ani ani-fade');
-//                    $(cardObj.target).css('opacity', 1);
-//                    $('body').removeClass('article-open');
-//                }, 300);
-//                setTimeout(function () {
-//                    $(cardObj.article).css({
-//                        'z-index': -2,
-//                        'opacity': 0
-//                    }).removeClass('ani ani-contract');
-//                }, 320);
-//            };
-            
-//            console.log(e.type,e.originalEvent.animationName);
-//            
-//            if (e.type == 'animationend' || e.type == 'webkitAnimationEnd') {
-//                console.log(e.originalEvent.animationName);
-//            };
-//        });
     }
 
 })(jQuery);
