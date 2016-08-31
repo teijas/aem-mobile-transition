e865293f0b43eb6bf3d7b8335aa316fee349e611
Teija Stearns <teija.stearns@6dglobal.com>
2016-08-31 16:50:47 -0700

    styles added for fullscreen functionality.

--- js/aem-mobile-transition.js
+++ js/aem-mobile-transition.js
@@ -1,16 +1,51 @@
 ;
 (function ($) {
-    var cardClone,cardObj;
+    var cardClone,cardObj,article;
     $.fn.aemTransition = function (options) {
         var opts = $.extend({}, $.fn.aemTransition.defaults, options);
         return this.each(function () {
-            $(this).bind('click', function () {
-                cardClone = $(this).clone().attr('id', 'cardClone');
+            if (opts.mode!='default') {
+                $(this).wrap('<div class="aem-wrapper"></div>')
+            }
+            $(this).bind(opts.event, function () {
                 cardObj = new Card(this,opts);
+                if (opts.mode=='default'){
+                    article=$(cardObj.article);
+                    cardClone = $(this).clone().attr('id', 'cardClone');
+                } else if (opts.mode!='default') {
+                    article=$(cardObj.target);
+                }
                 cardObj.makeKeyframes();
-                cardObj.aemExpand();
+                if (opts.mode=='default'){
+                    cardObj.aemExpand();
+                } else if (opts.mode!='default') {
+                    cardObj.fullscreen();
+                }
+                
+                $(cardObj.target).on('aem.expanded', function(e) {
+                    if (opts.mode=='default'){
+                        $(article).css({'z-index':1000,'opacity':1}).addClass('ani ani-appear');
+                    } else if (opts.mode!='default') {
+                        $(cardObj.relatedTarget).addClass('aem-fullscreen-wrapper bring-to-front ani ani-appear');
+                        $('#cardWrapper').css({
+                            'top': '',
+                            'width':'',
+                            'height':''
+                        }).addClass('aem-fullscreen-wrapper').removeClass('ani ani-expand');
+                        $(cardObj.target).addClass('vcenter');
+                        $(opts.back).fadeIn();
+                    }
+                });
                 $(opts.back).bind('click', function(e) {
-                    cardObj.aemContract();
+                    if (opts.mode=='default'){
+                        cardObj.aemContract();
+                    } else if (opts.mode!='default') {
+                        $(this).fadeOut();
+                        if (cardObj.mode=='video'){
+                            $(article)[0].pause();
+                        }
+                        cardObj.exitFullscreen();
+                    }
                 });
             });
         });
@@ -20,7 +55,9 @@
         back: '#backBtn',
         article: null,
         yOffset: 0,
-        xOffset: 0
+        xOffset: 0,
+        mode: 'default',
+        event: 'click'
     };
 
 
@@ -32,10 +69,12 @@
         var y = cords.top;
         var w = cords.width;
         var h = cords.height;
-        if (opts.article != null){
-            this.article= opts.article;
-        } else {
-            this.article= $($(myCard).attr('data-target'));
+        if (opts.mode=='default'){
+            if (opts.article != null){
+                this.article= opts.article;
+            } else {
+                this.article= $($(myCard).attr('data-target'));
+            }
         }
         this.q = $(myCard).position();
         this.hScale = winH / h;
@@ -48,7 +87,9 @@
         this.yTransBack = this.yTrans * -1;
         this.w = w;
         this.h = h;
-        this.target= myCard;
+        this.target = myCard;
+        this.relatedTarget = $(myCard).parent();
+        this.mode= opts.mode;
     }
     
     Card.prototype.makeKeyframes = function () {
@@ -68,6 +109,7 @@
     }
     
     Card.prototype.aemExpand = function () {
+        $(cardObj.target).trigger('aem.expand');
         var cardWrapper = $('<div class="ani ani-expand" id="cardWrapper"></div>').width(this.w).height(this.h).css('top', this.q.top);
         $(cardClone).appendTo(cardWrapper);
         $('body').addClass('article-open');
@@ -76,8 +118,24 @@
         $(cardClone).addClass('ani ani-fade');
         $('#cardWrapper').on('animationend webkitAnimationEnd',function(e){
             if(e.originalEvent.animationName=='canvas-expand'){
-                $(cardObj.article).css({'z-index':1000,'opacity':1}).addClass('ani ani-appear');
                 $(cardObj.target).trigger('aem.expanded');
+            };
+        });
+    }
+    
+    Card.prototype.fullscreen = function () {
+        $(article).trigger('aem.expand');
+        var cardWrapper = $('<div class="ani ani-expand" id="cardWrapper"></div>').css({
+            'top': this.q.top,
+            'background':'#000',
+            'width':this.w,
+            'height':this.h
+        });
+        $('body').addClass('article-open');
+        $(cardWrapper).insertAfter(this.relatedTarget);
+        $('#cardWrapper').on('animationend webkitAnimationEnd',function(e){
+            if(e.originalEvent.animationName=='canvas-expand'){
+                $(article).trigger('aem.expanded');
             };
         });
     }
@@ -99,13 +157,30 @@
                     }).removeClass('ani ani-contract');
                 }, 320);
             };
-            
-//            console.log(e.type,e.originalEvent.animationName);
-            
+                  
 //            if (e.type == 'animationend' || e.type == 'webkitAnimationEnd') {
 //                console.log(e.originalEvent.animationName);
 //            };
         });
+    }
+
+Card.prototype.exitFullscreen = function () {
+        $(this.relatedTarget).removeClass('ani ani-appear aem-fullscreen-wrapper bring-to-front').addClass('ani ani-fade');
+        $('#cardWrapper').addClass('ani ani-exit-fullscreen');
+        $(this.relatedTarget).on("webkitAnimationEnd animationend", function (e) {
+             if (e.originalEvent.animationName == 'fade-out'){
+                 setTimeout(function(){
+                     $(cardObj.target).removeClass('vcenter').fadeIn();
+                     $(cardObj.relatedTarget).removeClass('ani ani-fade');
+                     $('#cardWrapper').fadeOut();
+                     setTimeout(function(){
+                         $('#cardWrapper').remove();
+                     },200);
+                 }, 150);
+
+             }
+        });
+    
     }
 
 })(jQuery);
